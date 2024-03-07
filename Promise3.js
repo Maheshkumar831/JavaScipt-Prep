5c64a8d2d973258ff4e2856995b0440094cccbf4
Mahesh Kumar R <114941303+Maheshkumar831@users.noreply.github.com>
2024-03-07 15:07:26 +0530

    Create Promise3.js

---
+++ Promise3.js
@@ -0,0 +1,23 @@
+
+
+
+const user = fetch("https://jsonplaceholder.typicode.com/users").then((response) => {
+  return response.json()
+
+}).then(users=>{
+    users.forEach(use => {
+        console.log(use)
+    });
+});
+
+
+ername: 'Bret', email: 'Sincere@april.biz', address: {…}, …}
+script.js:11 {id: 2, name: 'Ervin Howell', username: 'Antonette', email: 'Shanna@melissa.tv', address: {…}, …}
+script.js:11 {id: 3, name: 'Clementine Bauch', username: 'Samantha', email: 'Nathan@yesenia.net', address: {…}, …}
+script.js:11 {id: 4, name: 'Patricia Lebsack', username: 'Karianne', email: 'Julianne.OConner@kory.org', address: {…}, …}
+script.js:11 {id: 5, name: 'Chelsey Dietrich', username: 'Kamren', email: 'Lucio_Hettinger@annie.ca', address: {…}, …}
+script.js:11 {id: 6, name: 'Mrs. Dennis Schulist', username: 'Leopoldo_Corkery', email: 'Karley_Dach@jasper.info', address: {…}, …}
+script.js:11 {id: 7, name: 'Kurtis Weissnat', username: 'Elwyn.Skiles', email: 'Telly.Hoeger@billy.biz', address: {…}, …}
+script.js:11 {id: 8, name: 'Nicholas Runolfsdottir V', username: 'Maxime_Nienow', email: 'Sherwood@rosamond.me', address: {…}, …}
+script.js:11 {id: 9, name: 'Glenna Reichert', username: 'Delphine', email: 'Chaim_McDermott@dana.io', address: {…}, …}
+script.js:11 {id: 10, name: 'Clementina DuBuque', username: 'Moriah.Stanton', email: 'Rey.Padberg@karina.biz', address: {…}, …}
