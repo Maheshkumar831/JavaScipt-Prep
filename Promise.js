0273b47b54d8494947a7ca28f2f677c0997db811
Mahesh Kumar R <114941303+Maheshkumar831@users.noreply.github.com>
2024-03-07 11:34:27 +0530

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -15,3 +15,26 @@ myPromise.then((value)=>{
 /*
 Promise { 'Yes resolved the promise' }
 Yes resolved the promise*/
+
+
+const myPromise=new Promise((resolve,reject)=>{
+    const error=true;
+    if(!error){
+        resolve("Yes resolved the promise");
+    }
+    else{
+        reject("reject the promise");
+    }
+});
+console.log(myPromise)
+myPromise.then((value)=>{
+  console.log(value)
+})
+.catch(err=>{
+      console.log(err);
+})
+
+/*
+Promise { <rejected> 'reject the promise' }
+reject the promise
+*/
